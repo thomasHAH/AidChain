76a058623b07147807f9d5e894626c32227cd7f1
seungjun-Lee0 <sjun0500@gmail.com>
2025-05-17 17:25:09 +1000

    chore: set up project dependencies and hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    }
+  },
+  paths: {
+    sources: "./",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
+}; 
